adee18c99d9d9d0ea4589dd51b692b57de96a9ad
sylvia villegas <sylvia@svillegas.com>
2017-02-21 18:44:33 -0500

    Create AJAX call to post on form submit

--- election.js
+++ election.js
@@ -13,9 +13,10 @@ $(document).ready(function() {
       $('<strong>').html(data.name + ': ').appendTo(listItem);
       $('<span>').html(data.votes + ' votes').appendTo(listItem);
 
-      var voteForm = $('<form>').css('margin', '10px 0 20px')
+      var voteForm = $('<form>');
       voteForm.attr('method', 'POST')
-              .attr('action', 'https://bb-election-api.herokuapp.com/vote');
+              .attr('action', 'https://bb-election-api.herokuapp.com/vote')
+              .css('margin', '10px 0 20px');
       $('<input>').attr('type', 'hidden')
                   .attr('name', 'id')
                   .attr('value', data.id)
@@ -26,7 +27,27 @@ $(document).ready(function() {
 
       voteForm.appendTo(listItem);
       $('#candidates').append(listItem);
+
+
+      $('form').on('submit', function(event) {
+        event.preventDefault();
+
+        $.ajax({
+          url: 'https://bb-election-api.herokuapp.com/vote',
+          method: 'POST',
+          data: { id: $(this).children('input[type=hidden]').val() } ,
+          // dataType: ''
+        }).done(function() {
+          console.log('success');
+        }).fail(function() {
+          console.log('failed');
+        });
+      });
     })
+
+
+
   });
+
 
 });
